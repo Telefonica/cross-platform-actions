1956b4bd408a04e73ee710bd70d76f254b782d67
javierbrea <javier.brea@gmail.com>
2023-04-25 13:58:29 +0200

    fix(CROSS-499): Run all tests

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: ["<rootDir>/test/unit/specs/*.spec.ts", "<rootDir>/test/unit/specs/**/*.test.ts"],
-  testMatch: ["<rootDir>/test/unit/**/Deploy.test.ts"],
+  // testMatch: ["<rootDir>/test/unit/**/Deploy.test.ts"],
 
   // The test environment that will be used for testing
   testEnvironment: "node",
