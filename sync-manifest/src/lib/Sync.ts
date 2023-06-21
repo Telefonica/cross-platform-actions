1ab66555da6b14698ab6937bfaa05e9228458a6e
Ismael Taboada Rodero <ismael.jtr@gmail.com>
2023-06-21 09:47:03 +0200

    fix(CROSS-1455): remove try/catch block

--- sync-manifest/src/lib/Sync.ts
+++ sync-manifest/src/lib/Sync.ts
@@ -10,14 +10,11 @@ export async function sync(inputs: SyncInputs, logger: Logger): Promise<string> 
   const secret = new Secret(inputs.secret, inputs.manifest, { logger });
   const createdSecrets = [];
   for (const { name, owner } of config.repositories) {
-    try {
-      logger.info(`Syncing ${name}...`);
-      const repository = new Repository(name, owner, inputs.token, { logger });
-      await repository.addSecret(secret);
-      createdSecrets.push({ repository: { name, owner }, secret: { name: secret.name } });
-    } catch (e) {
-      logger.error(`Failed to sync ${name}: ${(e as Error).toString()}`);
-    }
+    logger.info(`Syncing ${name}...`);
+    const repository = new Repository(name, owner, inputs.token, { logger });
+    await repository.addSecret(secret);
+    createdSecrets.push({ repository: { name, owner }, secret: { name: secret.name } });
+    logger.error(`Failed to sync ${name}: ${(e as Error).toString()}`);
   }
   return JSON.stringify({
     github: {
